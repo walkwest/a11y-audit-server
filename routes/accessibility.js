d19d797a11893251e125580b17d9d084ea280ded
Robert Povelones <rpovelones@walkwest.com>
2021-10-18 14:24:32 -0400

    Add CORS header to allow connections from any source

--- routes/accessibility.js
+++ routes/accessibility.js
@@ -6,7 +6,14 @@ var axePuppeteer = require('../modules/axe-puppeteer');
  * Scan endpoint to run axe audit on a website.
  */
 router.get('/scan/:url', function (req, res) {
-  
+
+    // CORS headers - (*) allow connection from any source.
+    // For better security, update this to only allow from trusted sources.
+    res.set('Access-Control-Allow-Origin', '*');
+
+    // AXE testing engine.
+    // Errors will cause the Node process to crash so it will need to be restarted or
+    // set to auto-restart.
     axePuppeteer(req.params.url)
         .then(results => {
             res.json(results)
